be882bdb0df8742e516c436b3bc5159454896ee5
Adam J. Berlier <ajberlier@gmail.com>
2023-10-03 09:57:50 -0400

    slurm scripts to run on cluster

---
+++ ada_scripts/launch_sbatch.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# define the experiments
+colnames=("algo" "env" "model" "save_interval" "frames")
+experiments=(
+    "ppo CartPole-v1 mlp 10 10000"
+) 
+storage='/home/aberlie1/ada_user/data/rl-starter-files/'
+source='/home/aberlie1/ada_user/miniconda3/etc/profile.d/conda.sh'
+
+# initialize an associative array
+declare -A column_indices
+
+# populate the associative array with column indices
+for i in "${!colnames[@]}"
+do
+    column_indices[${colnames[$i]}]=$i
+done
+
+# loop through the eperiment data rows
+for row in "${experiments[@]}"
+do
+    # split the row into an array
+    row_array=($row)
+
+    # access columns by header
+    algo=${row_array[${column_indices["algo"]}]}
+    env=${row_array[${column_indices["env"]}]}
+    model=${row_array[${column_indices["model"]}]}
+    save_interval=${row_array[${column_indices["save_interval"]}]}
+    frames=${row_array[${column_indices["frames"]}]}
+
+    # submit slurm job
+    sbatch sbatch_script.sh ${algo} ${env} ${model} ${save_interval} ${frames} ${source} ${storage}
+done
---
+++ ada_scripts/sbatch_script.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# set the input arguments
+ALGO=$1
+ENV=$2
+MODEL=$3
+SAVE_INTERVA=$4
+FRAME=$5
+CONDA=$6
+RL_STORAGE=$7
+
+# source conda environment
+source $SOURCE
+conda activate rl_env
+
+# set storage path environment variable
+export RL_STORAGE=$RL_STORAGE
+echo "RL_STORAGE=$RL_STORAGE"
+
+# print the input values
+echo "Algo: $algo, Env: $env, Model: $model, Save Interval: $save_interval, Frames: $frames"
+
+# run the training script
+python3 -m scripts.train --algo ALGO --env ENV --model MODEL --save-interval SAVE_INTERVAL --frames FRAMES
+
+# wait for all of the runs to complete before exiting
+wait

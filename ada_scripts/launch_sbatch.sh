256f2a094bbc71480922390c8177c57d5ded4046
Adam J. Berlier <ajberlier@gmail.com>
2023-10-28 16:55:51 -0400

    adding all environments to slurm script

--- ada_scripts/launch_sbatch.sh
+++ ada_scripts/launch_sbatch.sh
@@ -4,12 +4,46 @@
 script='/home/aberlie1/rl-starter-files/train.py'
 colnames=("algo" "env" "model" "save_interval" "frames")
 experiments=(
-    "ppo BabyAI-GoToObjS6-v0 BabyAI-GoToObj_PPO"
-    "ppo BabyAI-GoToLocalS8N7-v0 BabyAI-GoToLocal_PPO"
-    "ppo BabyAI-GoToObjMazeS7-v0 BabyAI-GoTo_PPO"
-    "ppo BabyAI-PickupDist BabyAI-PickupDist_PPO"
-    "ppo BabyAI-PutNextS7N4Carrying-v0 BabyAI-PutNext_PPO"
-    "ppo BabyAI-UnlockPickup BabyAI-UnlockPickup_PPO"
+    "ppo BabyAI-GoToRedBallGrey-v0 BabyAI-GoToRedBallGrey-v0_PPO”
+    "ppo BabyAI-GoToRedBall-v0 BabyAI-GoToRedBall-v0_PPO”
+    "ppo BabyAI-GoToRedBallNoDists-v0 BabyAI-GoToRedBallNoDists-v0_PPO”
+    "ppo BabyAI-GoToObjS6-v0 BabyAI-GoToObjS6-v0_PPO”
+    "ppo BabyAI-GoToLocalS8N7-v0 BabyAI-GoToLocalS8N7-v0_PPO”
+    "ppo BabyAI-GoToObjMazeS7-v0 BabyAI-GoToObjMazeS7-v0_PPO”
+    "ppo BabyAI-GoToImpUnlock-v0 BabyAI-GoToImpUnlock-v0_PPO”
+    "ppo BabyAI-GoToSeqS5R2-v0 BabyAI-GoToSeqS5R2-v0_PPO”
+    "ppo BabyAI-GoToRedBlueBall-v0 BabyAI-GoToRedBlueBall-v0_PPO”
+    "ppo BabyAI-GoToDoor-v0 BabyAI-GoToDoor-v0_PPO”
+    "ppo BabyAI-GoToObjDoor-v0 BabyAI-GoToObjDoor-v0_PPO”
+    "ppo BabyAI-Open-v0 BabyAI-Open-v0_PPO”
+    "ppo BabyAI-OpenRedDoor-v0 BabyAI-OpenRedDoor-v0_PPO”
+    "ppo BabyAI-OpenDoorLoc-v0 BabyAI-OpenDoorLoc-v0_PPO”
+    "ppo BabyAI-OpenRedBlueDoorsDebug-v0 BabyAI-OpenRedBlueDoorsDebug-v0_PPO”
+    "ppo BabyAI-OpenDoorsOrderN4Debug-v0 BabyAI-OpenDoorsOrderN4Debug-v0_PPO”
+    "ppo BabyAI-Pickup-v0 BabyAI-Pickup-v0_PPO”
+    "ppo BabyAI-UnblockPickup-v0 BabyAI-UnblockPickup-v0_PPO”
+    "ppo BabyAI-PickupLoc-v0 BabyAI-PickupLoc-v0_PPO”
+    "ppo BabyAI-PickupDistDebug-v0 BabyAI-PickupDistDebug-v0_PPO”
+    "ppo BabyAI-PickupAbove-v0 BabyAI-PickupAbove-v0_PPO”
+    "ppo BabyAI-PutNextLocalS6N4-v0 BabyAI-PutNextLocalS6N4-v0_PPO”
+    "ppo BabyAI-PutNextS7N4Carrying-v0 BabyAI-PutNextS7N4Carrying-v0_PPO”
+    "ppo BabyAI-Unlock-v0 BabyAI-Unlock-v0_PPO”
+    "ppo BabyAI-UnlockLocalDist-v0 BabyAI-UnlockLocalDist-v0_PPO”
+    "ppo BabyAI-KeyInBox-v0 BabyAI-KeyInBox-v0_PPO”
+    "ppo BabyAI-UnlockPickupDist-v0 BabyAI-UnlockPickupDist-v0_PPO”
+    "ppo BabyAI-BlockedUnlockPickup-v0 BabyAI-BlockedUnlockPickup-v0_PPO”
+    "ppo BabyAI-UnlockToUnlock-v0 BabyAI-UnlockToUnlock-v0_PPO”
+    "ppo BabyAI-ActionObjDoor-v0 BabyAI-ActionObjDoor-v0_PPO”
+    "ppo BabyAI-FindObjS7-v0 BabyAI-FindObjS7-v0_PPO”
+    "ppo BabyAI-KeyCorridorS6R3-v0 BabyAI-KeyCorridorS6R3-v0_PPO”
+    "ppo BabyAI-OneRoomS20-v0 BabyAI-OneRoomS20-v0_PPO”
+    "ppo BabyAI-MoveTwoAcrossS8N9-v0 BabyAI-MoveTwoAcrossS8N9-v0_PPO”
+    "ppo BabyAI-SynthS5R2-v0 BabyAI-SynthS5R2-v0_PPO”
+    "ppo BabyAI-SynthLoc-v0 BabyAI-SynthLoc-v0_PPO”
+    "ppo BabyAI-SynthSeq-v0 BabyAI-SynthSeq-v0_PPO”
+    "ppo BabyAI-MiniBossLevel-v0 BabyAI-MiniBossLevel-v0_PPO”
+    "ppo BabyAI-BossLevel-v0 BabyAI-BossLevel-v0_PPO”
+    "ppo BabyAI-BossLevelNoUnlock-v0 BabyAI-BossLevelNoUnlock-v0_PPO”
 ) 
 storage=/home/aberlie1/ada_user/data/rl-starter-files/
 conda=/home/aberlie1/ada_user/miniconda3/etc/profile.d/conda.sh

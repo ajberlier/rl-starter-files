d8aa7c8b41c15aafa627a1f0050d0361d3e91c7b
Adam J. Berlier <ajberlier@gmail.com>
2024-04-02 17:20:56 -0400

    minor agent updates to handle multiple architectures

--- utils/agent.py
+++ utils/agent.py
@@ -2,6 +2,8 @@ import torch
 
 import utils
 
+import torch_ac.algos.oc
+
 
 class Agent:
     """An agent.
@@ -10,26 +12,32 @@ class Agent:
     - to choose an action given an observation,
     - to analyze the feedback (i.e. reward and done state) of its action."""
 
-    def __init__(self, acmodel, argmax=False, num_envs=1):
+    def __init__(self, arch, argmax=False, num_envs=1):
 
-        self.acmodel = acmodel
+        self.arch = arch
         self.argmax = argmax
         self.num_envs = num_envs
 
-        if self.acmodel.recurrent:
-            self.memories = torch.zeros(self.num_envs, self.acmodel.memory_size, device=utils.device)
+        if self.arch.recurrent:
+            self.memories = torch.zeros(self.num_envs, self.arch.memory_size, device=utils.device)
 
-        self.acmodel.to(utils.device)
-        self.acmodel.eval()
+        self.arch.to(utils.device)
+        self.arch.eval()
 
     def get_actions(self, obss):
-        preprocessed_obss = self.acmodel.preprocess_obss(obss, device=utils.device)
+        preprocessed_obss = self.arch.preprocess_obss(obss, device=utils.device)
 
         with torch.no_grad():
-            if self.acmodel.recurrent:
-                dist, _, self.memories = self.acmodel(preprocessed_obss, self.memories)
+            if self.arch.recurrent:
+                if self.arch.__class__.__name__ == 'OCModel':
+                    dist, _, option_dist, self.memories = self.arch(preprocessed_obss, self.memories)
+                elif self.arch.__class__.__name__ == 'ACModel':
+                    dist, _, self.memories = self.arch(preprocessed_obss, self.memories)
             else:
-                dist, _ = self.acmodel(preprocessed_obss)
+                if self.arch.__class__.__name__ == 'OCModel':
+                    dist, _, option_dist = self.arch(preprocessed_obss)
+                elif self.arch.__class__.__name__ == 'ACModel':    
+                    dist, _ = self.arch(preprocessed_obss)
 
         if self.argmax:
             actions = dist.probs.max(1, keepdim=True)[1]
@@ -42,7 +50,7 @@ class Agent:
         return self.get_actions([obs])[0]
 
     def analyze_feedbacks(self, rewards, dones):
-        if self.acmodel.recurrent:
+        if self.arch.recurrent:
             masks = 1 - torch.tensor(dones, dtype=torch.float, device=utils.device).unsqueeze(1)
             self.memories *= masks
 
